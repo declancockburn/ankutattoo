280f804eac0c485d69fa00b7f92bd6176bf519dd
declancockburn <apexaviour@gmail.com>
2021-01-26 23:57:11 +0100

    made gallery into rows

--- _image_resize/main.py
+++ _image_resize/main.py
@@ -28,6 +28,45 @@ for file in files:
 
 
 
+## Crop already existing height to 300px
+
+orig = Path(r"C:\Users\Declan\Desktop\Website_Trials\ankutattoo\assets\img\gallery")
+root = Path(r"C:\Users\Declan\Desktop\Website_Trials\ankutattoo")
+thumbs = Path(r"C:\Users\Declan\Desktop\Website_Trials\ankutattoo\assets\img\gallery_thumbs")
+thumbs_sqr = Path(r"C:\Users\Declan\Desktop\Website_Trials\ankutattoo\assets\img\gallery_thumbs_sqr")
+
+thumbs_walk = thumbs.glob('**/*')
+
+files = [x for x in thumbs_walk if x.is_file()]
+files = [x for x in files if x.suffix == '.jpg']
+
+for file in files:
+# file = files[0]
+    img = Image.open(file)
+    w, h = img.size
+    if h > 300:
+        delta = h - 300
+        top = delta // 2
+        bottom = delta // 2
+        if delta % 2 == 1:
+            bottom += 1
+        img_cropped = img.crop((0, top, w, h-bottom))
+    else:
+        img_cropped = img.crop((0, 0, w, h))
+    rel_path = file.relative_to(thumbs)
+    thumb_sqr_file = thumbs_sqr.joinpath(rel_path)
+    thumb_sqr_file.parent.mkdir(parents=True, exist_ok=True)
+    img_cropped.save(thumb_sqr_file)
+    main_root = orig.joinpath(file.relative_to(thumbs))
+
+    rel_main_root = main_root.relative_to(root)
+    rel_thumb_root = thumb_sqr_file.relative_to(root)
+    print(f"  - image_path: /{rel_main_root.as_posix()}\n"
+          f"    thumb_path: /{rel_thumb_root.as_posix()}\n"
+          f"    name: {file.stem}")
+
+
+
 
 
 
